cc27e4757a9bfd0886aa277f34c5f7fd756ac3bc
Neil Campbell <neildcampbell@hotmail.com>
2019-12-09 10:59:53 +0000

    added new tests for powers

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -2,18 +2,29 @@
 # --require spec-helper
 # --require color
 
-  def convert_maths_string(input)
-    a = input.split(" ")
-    if a[1] === '+'
-     answer =  a[0].to_i + a[2].to_i 
-        elsif a[1] === 'x' 
-            answer =  a[0].to_i * a[2].to_i
-        elsif a[1] === '-'    
-            answer =  a[0].to_i - a[2].to_i
-        elsif a[1] === '/'    
-            answer =  a[0].to_i / a[2].to_i
-        elsif a.length != 3    
-            answer =  "wrong format"
-    end 
-        [input, answer]    
+def powers_of_3(input) 
+    i=0
+        loop do 
+            num =  3 ** i 
+            i=i+1
+            if num >= input 
+                return i-2
+             break
+            end
+        end
 end
+#   def convert_maths_string(input)
+#     a = input.split(" ")
+#     if a[1] === '+'
+#      answer =  a[0].to_i + a[2].to_i 
+#         elsif a[1] === 'x' 
+#             answer =  a[0].to_i * a[2].to_i
+#         elsif a[1] === '-'    
+#             answer =  a[0].to_i - a[2].to_i
+#         elsif a[1] === '/'    
+#             answer =  a[0].to_i / a[2].to_i
+#         elsif a.length != 3    
+#             answer =  "wrong format"
+#     end 
+#         [input, answer]    
+# end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,26 +1,59 @@
 require 'roman_numerals'
 
-describe "Maths String calculator" do
-  it "return 2 when given 1 + 1" do
-    input = "1 + 1"
-    expect(convert_maths_string(input)).to eq ["1 + 1",2]
+describe "powers of 3" do 
+  it "returns 1 when given 2 as input" do
+    input = 3
+    result = powers_of_3(input)
+    expect(result).to eq (0)
   end
-  it "return 3 when given 1 x 3" do
-    input = "1 x 3"
-    expect(convert_maths_string(input)).to eq ["1 x 3",3]
+  it "returns 1 when given 4 as input" do
+      input = 5
+      result = powers_of_3(input)
+      expect(result).to eq (1)
+    end
+  it "returns 1 when given 4 as input" do
+    input = 5
+    result = powers_of_3(input)
+    expect(result).to eq (1)
   end
-  it "return 3 when given 8 - 5" do
-    input = "8 - 5"
-    expect(convert_maths_string(input)).to eq ["8 - 5",3]
+  it "returns 2 when given 10 as input" do
+    input = 7
+    result = powers_of_3(input)
+    expect(result).to eq (1)
   end
-  it "return 2 when given 10 / 5" do
-    input = "10 / 5"
-    expect(convert_maths_string(input)).to eq ["10 / 5",2]
+  it "returns 3 when given 81 as input" do
+    input = 81
+    result = powers_of_3(input)
+    expect(result).to eq (3)
   end
-  it "return wrong format if length not 3" do
-    input = "10/5"
-    expect(convert_maths_string(input)).to eq ["10/5","wrong format"]
+  it "returns 82 when given 4 as input" do
+    input = 82
+    result = powers_of_3(input)
+    expect(result).to eq (4)
   end
 end
 
+# describe "Maths String calculator" do
+#   it "return 2 when given 1 + 1" do
+#     input = "1 + 1"
+#     expect(convert_maths_string(input)).to eq ["1 + 1",2]
+#   end
+#   it "return 3 when given 1 x 3" do
+#     input = "1 x 3"
+#     expect(convert_maths_string(input)).to eq ["1 x 3",3]
+#   end
+#   it "return 3 when given 8 - 5" do
+#     input = "8 - 5"
+#     expect(convert_maths_string(input)).to eq ["8 - 5",3]
+#   end
+#   it "return 2 when given 10 / 5" do
+#     input = "10 / 5"
+#     expect(convert_maths_string(input)).to eq ["10 / 5",2]
+#   end
+#   it "return wrong format if length not 3" do
+#     input = "10/5"
+#     expect(convert_maths_string(input)).to eq ["10/5","wrong format"]
+#   end
+# end
+# end
 
